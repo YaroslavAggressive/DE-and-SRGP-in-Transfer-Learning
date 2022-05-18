d98c76046b1b49aca52dc5394444e027c160e7c5
ProjectTPP <tyrykin.yaa@edu.spbstu.ru>
2022-05-18 21:15:41 +0300

    Removed datasets without format and added correct versions

--- dataset_preprocessing.py
+++ dataset_preprocessing.py
@@ -2,6 +2,8 @@ from dataset_parsing import initial_parse_data_and_save, MERGED_DATASET
 from dataset_parsing import get_data_response, parse_per_snp, parse_per_season, parse_per_key, DATASET_SEASONS
 import pandas as pd
 
+CSV_SUFFIX = ".csv"
+
 initial_parse_data_and_save()
 
 merged_dataset = pd.read_csv(MERGED_DATASET, sep=';')
@@ -13,18 +15,18 @@ del merged_dataset[redundant_column_name]
 for i in range(6):
     snp_ind = i + 1
     dataset_data = parse_per_snp(merged_dataset, y_merged, snp_ind)
-    dataset_data[0].to_csv("snp_datasets/data_no_snp_" + str(snp_ind), sep=';', index=False)
-    dataset_data[1].to_csv("snp_datasets/data_no_snp_" + str(snp_ind) + "_response", sep=';', index=False)
+    dataset_data[0].to_csv("snp_datasets/data_no_snp_" + str(snp_ind) + CSV_SUFFIX, sep=';', index=False)
+    dataset_data[1].to_csv("snp_datasets/data_no_snp_" + str(snp_ind) + "_response" + CSV_SUFFIX, sep=';', index=False)
 
 # creating datasets per season
 for season in DATASET_SEASONS.keys():
     dataset_data = parse_per_season(merged_dataset, y_merged, season)
-    dataset_data[0].to_csv("seasons_datasets/data_season_" + season, sep=';', index=False)
-    dataset_data[1].to_csv("seasons_datasets/data_season_" + season + "_response", sep=';', index=False)
+    dataset_data[0].to_csv("seasons_datasets/data_season_" + season + CSV_SUFFIX, sep=';', index=False)
+    dataset_data[1].to_csv("seasons_datasets/data_season_" + season + "_response" + CSV_SUFFIX, sep=';', index=False)
 
 # creating datasets per geo_id
 
 for geo_id in [0., 2., 3.]:
     dataset_data = parse_per_key(merged_dataset, y_merged, geo_id, "geo_id")
-    dataset_data[0].to_csv("geo_datasets/data_geo_" + str(geo_id), sep=';', index=False)
-    dataset_data[1].to_csv("geo_datasets/data_geo_" + str(geo_id) + "_response", sep=';', index=False)
+    dataset_data[0].to_csv("geo_datasets/data_geo_" + str(geo_id) + CSV_SUFFIX, sep=';', index=False)
+    dataset_data[1].to_csv("geo_datasets/data_geo_" + str(geo_id) + "_response" + CSV_SUFFIX, sep=';', index=False)
