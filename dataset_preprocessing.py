7edd9ad260597a13ea694fc920bce8ab4524a78a
ProjectTPP <tyrykin.yaa@edu.spbstu.ru>
2022-05-18 21:11:33 +0300

    Update datasets separation

--- dataset_preprocessing.py
+++ dataset_preprocessing.py
@@ -13,18 +13,18 @@ del merged_dataset[redundant_column_name]
 for i in range(6):
     snp_ind = i + 1
     dataset_data = parse_per_snp(merged_dataset, y_merged, snp_ind)
-    dataset_data[0].to_csv("snp_datasets/data_no_snp_" + str(snp_ind))
-    dataset_data[1].to_csv("snp_datasets/data_no_snp_" + str(snp_ind) + "_response")
+    dataset_data[0].to_csv("snp_datasets/data_no_snp_" + str(snp_ind), sep=';', index=False)
+    dataset_data[1].to_csv("snp_datasets/data_no_snp_" + str(snp_ind) + "_response", sep=';', index=False)
 
 # creating datasets per season
 for season in DATASET_SEASONS.keys():
     dataset_data = parse_per_season(merged_dataset, y_merged, season)
-    dataset_data[0].to_csv("seasons_datasets/data_season_" + season)
-    dataset_data[1].to_csv("seasons_datasets/data_season_" + season + "_response")
+    dataset_data[0].to_csv("seasons_datasets/data_season_" + season, sep=';', index=False)
+    dataset_data[1].to_csv("seasons_datasets/data_season_" + season + "_response", sep=';', index=False)
 
 # creating datasets per geo_id
 
 for geo_id in [0., 2., 3.]:
     dataset_data = parse_per_key(merged_dataset, y_merged, geo_id, "geo_id")
-    dataset_data[0].to_csv("geo_datasets/data_geo_" + str(geo_id))
-    dataset_data[1].to_csv("geo_datasets/data_geo_" + str(geo_id) + "_response")
+    dataset_data[0].to_csv("geo_datasets/data_geo_" + str(geo_id), sep=';', index=False)
+    dataset_data[1].to_csv("geo_datasets/data_geo_" + str(geo_id) + "_response", sep=';', index=False)
